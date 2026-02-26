7b9a3f0e81e4eba6d77882bef5b60c1fb6ad4498
Devon Tyson <devonltyson@outlook.com>
2026-02-25 21:48:02 -0500

    dupe import

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import '../theme.dart';
